16f078a84d405b5324dcc6af65abdfbb4664e770
Developer <dev@scoreboard.local>
2026-03-28 14:44:10 +0100

    test: add failing test for updating score

--- src/test/java/com/scoreboard/ScoreboardTest.java
+++ src/test/java/com/scoreboard/ScoreboardTest.java
@@ -20,4 +20,17 @@ public class ScoreboardTest {
         assertEquals(0, match.getHomeScore());
         assertEquals(0, match.getAwayScore());
     }
+
+    @Test
+    public void testUpdateScore_absoluteScores() {
+        Scoreboard scoreboard = new Scoreboard();
+        scoreboard.startMatch("Mexico", "Canada");
+        scoreboard.updateScore("Mexico", "Canada", 1, 0);
+
+        List<Match> summary = scoreboard.getSummary();
+        assertEquals(1, summary.size());
+        Match match = summary.get(0);
+        assertEquals(1, match.getHomeScore());
+        assertEquals(0, match.getAwayScore());
+    }
 }
